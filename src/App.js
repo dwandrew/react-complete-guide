79f7d78526943f2d77ecb2f299bb0a1eecf0b38e
dwandrew <danwandrew@gmail.com>
2020-12-16 11:10:09 +0000

    adding setState etc

--- src/App.js
+++ src/App.js
@@ -13,10 +13,22 @@ class App extends Component {
 
     ]
   }
+
+  changeNameHandler = () =>{
+    this.setState(
+      {people: [
+        {name: "Devin", age: 22},
+        {name: "Charlie", age: 28},
+        {name: "Penelope", age: 31}
+      ]}
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React app</h1>
+        <button onClick = {this.changeNameHandler}>Change name</button>
         <Person name = {this.state.people[0].name} age ={this.state.people[0].age}/>
         <Person name = {this.state.people[1].name} age ={this.state.people[1].age}/>
         <Person name = {this.state.people[2].name} age ={this.state.people[2].age}/>
