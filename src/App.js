39f9b8fbf50fadde66f670e5f017fe150e8259c7
dwandrew <danwandrew@gmail.com>
2020-12-16 10:01:14 +0000

    added basic person component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Person from './Components/Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>React app</h1>
+        <Person />
       </div>
     );
   }
