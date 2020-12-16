1b6edca8865c7dd40df1c460b78b093bf754be44
dwandrew <danwandrew@gmail.com>
2020-12-16 11:01:48 +0000

    adding basic state to app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import './App.css';
 import Person from './Components/Person/Person'
 
 class App extends Component {
+  state  = {
+    people: [
+      {name: "Brian", age: 22},
+      {name: "Charlie", age: 28},
+      {name: "Penelope", age: 31}
+
+
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>React app</h1>
-        <Person name = "Brian" age ="22"/>
-        <Person name = "Charlie" age ="28"/>
-        <Person name = "Penelope" age ="31"/>
+        <Person name = {this.state.people[0].name} age ={this.state.people[0].age}/>
+        <Person name = {this.state.people[1].name} age ={this.state.people[1].age}/>
+        <Person name = {this.state.people[2].name} age ={this.state.people[2].age}/>
+
       
       </div>
     );
