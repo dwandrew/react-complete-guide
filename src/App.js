88d7a10a116c4adc8834070e47db174b62fd0ef3
dwandrew <danwandrew@gmail.com>
2020-12-16 13:13:37 +0000

    changed to add hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import Person from './Components/Person/Person'
 
-class App extends Component {
-  state  = {
+const App = (props) => {
+
+  const [peopleState, setPeople] = useState({
     people: [
       {name: "Brian", age: 22},
       {name: "Charlie", age: 28},
@@ -12,31 +13,31 @@ class App extends Component {
 
 
     ]
-  }
+  })
 
-  changeNameHandler = () =>{
-    this.setState(
+
+  const changeNameHandler = () =>{
+    setPeople(
       {people: [
         {name: "Devin", age: 22},
         {name: "Charlie", age: 28},
         {name: "Penelope", age: 31}
+
       ]}
     )
   }
 
-  render() {
-    return (
+  return (
       <div className="App">
         <h1>React app</h1>
-        <button onClick = {this.changeNameHandler}>Change name</button>
-        <Person name = {this.state.people[0].name} age ={this.state.people[0].age}/>
-        <Person name = {this.state.people[1].name} age ={this.state.people[1].age}/>
-        <Person name = {this.state.people[2].name} age ={this.state.people[2].age}/>
+        <button onClick = {changeNameHandler}>Change name</button>
+        <Person name = {peopleState.people[0].name} age ={peopleState.people[0].age}/>
+        <Person name = {peopleState.people[1].name} age ={peopleState.people[1].age}/>
+        <Person name = {peopleState.people[2].name} age ={peopleState.people[2].age}/>
 
       
       </div>
     );
-  }
 }
 
 export default App;
